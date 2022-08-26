5ff00dc06d7b61308e6baad504cd9bca220d67de
Fisher Medders <fishermedders@gmail.com>
2022-08-26 12:04:19 -0400

    commit dos

--- fmpack.lua
+++ fmpack.lua
@@ -14,7 +14,13 @@ local basalt = require("/lib/basalt")
 
 local main = basalt.createFrame()
 
-local list = main:addList():setSize("parent.w-2", "parent.h-2"):setPosition(2,2)
+local list = main:addList():setSize("parent.w-2", "parent.h-6"):setPosition(2,2)
+local install = main:addButton("install")
+    :setSize("parent.w/4-2","3")
+    :setPosition(2, "parent.h-3")
+    :setText("Install")
+
+local text = main:addTextarea():setSize()
 
 local repoStr = http.get("https://api.github.com/search/repositories?q=topic:computercraft&sort=stars&order=desc").readAll()
 local repos = json.decode(repoStr)
